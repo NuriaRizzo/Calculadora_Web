8fb724d417a7edfdf3a2aca5422c7d1b6505fc26
NuriaRizzo <nrizzo2550@utm.edu.ec>
2026-05-12 19:27:27 -0500

    edit js

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-// Obtiene el elemento pantalla usando su ID
+// Selecciona la pantalla
 const pantalla = document.getElementById("pantalla");
 
 
-// Función para agregar números u operadores
+// Función para agregar valores
 function agregar(valor){
 
-    // Concatena el valor presionado a la pantalla
     pantalla.value += valor;
 }
 
 
-// Función para limpiar toda la pantalla
+// Función limpiar
 function limpiar(){
 
-    // Vacía el contenido de la pantalla
     pantalla.value = "";
 }
 
 
-// Función para borrar un carácter
+// Función borrar último carácter
 function borrar(){
 
-    // slice(0, -1) elimina el último carácter
     pantalla.value = pantalla.value.slice(0, -1);
 }
 
 
-// Función que realiza el cálculo
+// Función calcular
 function calcular(){
 
-    // try intenta ejecutar el código
     try{
 
-        // eval interpreta la operación matemática
         pantalla.value = eval(pantalla.value);
 
-    // catch captura errores
     }catch(error){
 
-        // Si existe un error muestra "Error"
         pantalla.value = "Error";
     }
 }
